df9beb6cda5e4335ce6b68ff70c2dc7c73a9a7ab
kanha-ks <kanha.namdev@ksolves.com>
2026-03-11 02:16:52 +0530

    adding custom task

--- controllers/model_data_controller.py
+++ controllers/model_data_controller.py
@@ -1,5 +1,7 @@
 from odoo import http
 from odoo.http import request
+from odoo.orm.fields_misc import Json
+
 
 class ModelDataController(http.Controller):
 
@@ -19,3 +21,29 @@ class ModelDataController(http.Controller):
         return request.render("device_management.model_view_template", {
             'models' : model_name
         })
+
+    @http.route('/website_sale/addModuleInfo', type='json',  website=True, auth='public')
+    def modelSpecifcDataShown(self, model_name):
+        print(f"The model I recieved is {model_name}")
+
+        # records = request.env[model_name].sudo().search_read([], [])
+
+        # Fetch the model metadata
+        model = request.env['ir.model'].sudo().search([('model', '=', model_name)])
+
+        info = {
+            'id': model.id,
+            'name': model.name,
+            'model': model.model,
+            'module': model.module,
+            'state': model.state,
+            'transient': model.transient,
+            'access_ids': [
+                (access.id, access.name, access.perm_read, access.perm_write, access.perm_create, access.perm_unlink)
+                for access in model.access_ids],
+            'field_ids': [(field.id, field.name, field.ttype) for field in model.field_ids],
+        }
+
+        return info
+        # return records
+
--- static/src/js/show_model_info.js
+++ static/src/js/show_model_info.js
@@ -1,12 +1,14 @@
+/** @odoo-module **/
+
 import { Interaction } from '@web/public/interaction';
 import { registry } from '@web/core/registry';
-
+import {        rpc } from "@web/core/network/rpc";
 
 export class ShowModelInfo extends Interaction {
     static selector = '.website_model_view';
 
     dynamicContent = {
-        'select.js_model_click' : {'t-on-click': this.showModelInformation},
+        'select.js_model_click' : {'t-on-change': this.showModelInformation},
     };
 
         setup() {
@@ -16,8 +18,18 @@ export class ShowModelInfo extends Interaction {
             console.log("Start func");
         }
 
-    showModelInformation(){
-        console.log("Hey Selector worked !!")
+    async showModelInformation(ev){
+        console.log(ev.target.value); //model name
+        let model_name = ev.target.value;
+
+            const modelNameInfo = await rpc(
+                '/website_sale/addModuleInfo',
+                {
+                    model_name : model_name,
+                }
+            );
+
+        console.log(modelNameInfo)
     }
 }
 
